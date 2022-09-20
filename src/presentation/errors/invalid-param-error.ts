bd4388ed2f61adbfb1f1a899b00bacfd2a403426
rafael altieris <raphaelaltieris@gmail.com>
2022-09-20 17:25:34 -0300

    feat: ensure SignUpController return 400 if an invalid email is provided

---
+++ src/presentation/errors/invalid-param-error.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-template-curly-in-string */
+export class InvalidParamError extends Error {
+  constructor (paramName: string) {
+    super('Invalid param: ${paramName}')
+    this.name = 'InvalidParamError'
+  }
+}
---
+++ src/presentation/protocols/emailValidator.ts
@@ -0,0 +1,4 @@
+export interface EmailValidator {
+  // eslint-disable-next-line @typescript-eslint/method-signature-style
+  isValid (email: string): boolean
+}
