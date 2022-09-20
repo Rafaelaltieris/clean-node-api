7d918cb24dab9719ce52e1e728bfbddec93e4581
rafael altieris <raphaelaltieris@gmail.com>
2022-09-20 14:33:56 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -12,6 +12,19 @@ describe('Signup Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+})
+test('Should return 400 if no email is provided', () => {
+  const sut = new SignUpController()
+  const httpRequest = {
+    body: {
+      email: 'any_email@email.com',
+      password: 'any_password',
+      passwordConfirmation: 'any_password'
+    }
+  }
+  const httpResponse = sut.handle(httpRequest)
+  expect(httpResponse.statusCode).toBe(400)
+  expect(httpResponse.body).toEqual(new Error('Missing param: email'))
 })
