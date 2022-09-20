f9fe041b9c3f409d3e69351afaaa6cedcaa40b77
rafael altieris <raphaelaltieris@gmail.com>
2022-09-20 16:45:34 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,8 +1,13 @@
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
+
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
+
 describe('Signup Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -16,7 +21,7 @@ describe('Signup Controller', () => {
   })
 })
 test('Should return 400 if no email is provided', () => {
-  const sut = new SignUpController()
+  const sut = makeSut()
   const httpRequest = {
     body: {
       email: 'any_email@email.com',
@@ -30,7 +35,7 @@ test('Should return 400 if no email is provided', () => {
 })
 
 test('Should return 400 if no password is provided', () => {
-  const sut = new SignUpController()
+  const sut = makeSut()
   const httpRequest = {
     body: {
       name: 'any_name',
@@ -44,7 +49,7 @@ test('Should return 400 if no password is provided', () => {
 })
 
 test('Should return 400 if no password confirmation is provided', () => {
-  const sut = new SignUpController()
+  const sut = makeSut()
   const httpRequest = {
     body: {
       name: 'any_name',
