21a502be7a093d33e6484102336cadead2240b87
rafael altieris <raphaelaltieris@gmail.com>
2022-09-21 20:47:03 -0300

    feat: ensure SignUpController call AddAccount with correct values

---
+++ src/domain/models/account.ts
@@ -0,0 +1,6 @@
+export interface AccountModel {
+  id: string
+  name: String
+  email: string
+  password: string
+}
---
+++ src/domain/usecase/add_account.ts
@@ -0,0 +1,11 @@
+import { AccountModel } from '../models/account'
+
+export interface AddAccountModel {
+  name: String
+  email: string
+  password: string
+}
+
+export interface AddAccount {
+  add: (account: AddAccountModel) => AccountModel
+}
