7590986eb2f125ad34abd0f84dd7e1288340d312
rafael altieris <raphaelaltieris@gmail.com>
2022-09-21 09:03:10 -0300

    test: ensure SignUpController call EmailValidator with correct email

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -13,7 +13,7 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    const requiredFields = ['name', 'em ail', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
