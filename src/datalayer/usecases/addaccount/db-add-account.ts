ac205bc3760a62f3bc02dcefc4a3cbd1e0f29dcd
rafael altieris <raphaelaltieris@gmail.com>
2022-09-23 10:17:51 -0300

    feat: ensure DbAddAccount call encrypter with correct password

---
+++ src/datalayer/usecases/addaccount/db-add-account.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/return-await */
+import { AccountModel } from '../../../domain/models/account'
+import { AddAccount, AddAccountModel } from '../../../domain/Usecase/add_account'
+import { Encrypter } from '../../protocols/encypter'
+
+export class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter
+
+  constructor (encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    await this.encrypter.encrypt(account.password)
+    return new Promise(resolve => resolve(null))
+  }
+}
