7aa0162640903363500139c9d1cd0f7d28706c9e
rafael altieris <raphaelaltieris@gmail.com>
2022-09-23 10:33:29 -0300

    test: ensure DbAddAccount throws if Encrypter throws

--- src/datalayer/usecases/addaccount/db-add-account.spec.ts
+++ src/datalayer/usecases/addaccount/db-add-account.spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/return-await */
 import { DbAddAccount } from './db-add-account'
-import { Encrypter } from '../../protocols/encypter'
+import { Encrypter } from '../../protocols/encrypter'
 
 interface SutTypes {
   sut: DbAddAccount
@@ -36,5 +36,17 @@ describe('DbAddAccount Usecase', () => {
     }
     await sut.add(accountData)
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
+  })
+
+  test('Should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
   })
 })
--- src/datalayer/usecases/addaccount/db-add-account.ts
+++ src/datalayer/usecases/addaccount/db-add-account.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/return-await */
 import { AccountModel } from '../../../domain/models/account'
 import { AddAccount, AddAccountModel } from '../../../domain/Usecase/add_account'
-import { Encrypter } from '../../protocols/encypter'
+import { Encrypter } from '../../protocols/encrypter'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
