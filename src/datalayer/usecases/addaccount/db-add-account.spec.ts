29cee42cec82d3dfe0c5870b15531c8af4664b6e
rafael altieris <raphaelaltieris@gmail.com>
2022-09-23 11:52:44 -0300

    test: ensure DbAccount returns an account on succes

--- src/datalayer/usecases/addaccount/db-add-account.spec.ts
+++ src/datalayer/usecases/addaccount/db-add-account.spec.ts
@@ -95,3 +95,19 @@ describe('DbAddAccount Usecase', () => {
     await expect(promise).rejects.toThrow()
   })
 })
+
+test('Should return an account on sucess', async () => {
+  const { sut } = makeSut()
+  const accountData = {
+    name: 'valid_name',
+    email: 'valid_email',
+    password: 'valid_password'
+  }
+  const account = await sut.add(accountData)
+  expect(account).toEqual({
+    id: 'valid_id',
+    name: 'valid_name',
+    email: 'valid_email',
+    password: 'hashed_password'
+  })
+})
