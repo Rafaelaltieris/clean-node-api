0934c0fd6a669b00cbb1e83ce419c56715f24f03
rafael altieris <raphaelaltieris@gmail.com>
2022-09-28 17:15:22 -0300

    test: ensure AccountMongoRepository returns an account on succes

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,10 +1,10 @@
 import { MongoHelper } from './helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
-
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
@@ -19,7 +19,7 @@ describe('Account Mongo Repository', () => {
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email')
+    expect(account.email).toBe('any_email@email.com')
     expect(account.password).toBe('any_password')
   })
 })
