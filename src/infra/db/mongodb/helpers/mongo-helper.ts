8728a3f61228568151dd73ef93bcfd473958f174
rafael altieris <raphaelaltieris@gmail.com>
2022-10-04 10:41:14 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,22 +2,26 @@ import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+  url: null as string,
 
   async connect (url: string): Promise<void> {
+    this.url = url
     this.client = await MongoClient.connect(url, {
     })
   },
 
   async disconnect (): Promise<void> {
     await this.client?.close()
+    this.client = null
   },
 
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    await this.connect(this.url)
     return this.client.db().collection(name)
   },
 
   map: async (collection: any): Promise<any> => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne({ ...collection })
     return { ...collection, id: (result).insertedId.toString() }
   }
